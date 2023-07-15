571dd858eb78072bf1593ae4ba949389ca4f0e88
adolf <135438640+bars1k1337@users.noreply.github.com>
2023-07-15 16:56:23 +0300

    add draw

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -15,6 +15,7 @@ clicked = False
 pos = []
 player = 1
 winner = 0
+tie = False
 game_over = False
 
 green = (0, 255, 0)
@@ -23,8 +24,7 @@ blue = (0, 0, 255)
 
 font = pygame.font.SysFont(None, 40)
 
-again_rect = Rect(screen_width // 2 - 80 , screen_height // 2, 160, 50)
-
+again_rect = Rect(screen_width // 2 - 80, screen_height // 2, 160, 50)
 
 
 def draw_grid():
@@ -35,9 +35,11 @@ def draw_grid():
         pygame.draw.line(screen, grid, (0, x * 100), (screen_width, x * 100), line_width)
         pygame.draw.line(screen, grid, (x * 100, 0), (x * 100, screen_height), line_width)
 
+
 for x in range(3):
     row = [0] * 3
     markers.append(row)
+
 
 def draw_markers():
     x_pos = 0
@@ -45,17 +47,20 @@ def draw_markers():
         y_pos = 0
         for y in x:
             if y == 1:
-                pygame.draw.line(screen, green, (x_pos * 100 + 15, y_pos * 100 + 15), (x_pos * 100 + 85, y_pos * 100 + 85), line_width)
-                pygame.draw.line(screen, green, (x_pos * 100 + 15, y_pos * 100 + 85), (x_pos * 100 + 85, y_pos * 100 + 15), line_width)
+                pygame.draw.line(screen, green, (x_pos * 100 + 15, y_pos * 100 + 15),
+                                 (x_pos * 100 + 85, y_pos * 100 + 85), line_width)
+                pygame.draw.line(screen, green, (x_pos * 100 + 15, y_pos * 100 + 85),
+                                 (x_pos * 100 + 85, y_pos * 100 + 15), line_width)
             if y == -1:
                 pygame.draw.circle(screen, red, (x_pos * 100 + 50, y_pos * 100 + 50), 38, line_width)
             y_pos += 1
         x_pos += 1
 
-def check_winner():
 
+def check_winner():
     global winner
     global game_over
+    global tie
     y_pos = 0
     for x in markers:
         if sum(x) == 3:
@@ -79,6 +84,11 @@ def check_winner():
         winner = 2
         game_over = True
 
+    if all(markers[i][j] != 0 for i in range(3) for j in range(3)):
+        tie = True
+        game_over = True
+
+
 def draw_winner(winner):
     win_text = "Player " + str(winner) + " wins!"
     win_img = font.render(win_text, True, blue)
@@ -90,6 +100,19 @@ def draw_winner(winner):
     pygame.draw.rect(screen, green, again_rect)
     screen.blit(again_img, (screen_width // 2 - 80, screen_height // 2 + 10))
 
+
+def draw_tie():
+    tie_text = "It's a tie!"
+    tie_img = font.render(tie_text, True, blue)
+    pygame.draw.rect(screen, green, (screen_width // 2 - 80, screen_height // 2 - 60, 160, 50))
+    screen.blit(tie_img, (screen_width // 2 - 70, screen_height // 2 - 50))
+
+    again_text = "Play again?"
+    again_img = font.render(again_text, True, blue)
+    pygame.draw.rect(screen, green, again_rect)
+    screen.blit(again_img, (screen_width // 2 - 80, screen_height // 2 + 10))
+
+
 run = True
 while run:
 
@@ -99,20 +122,25 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if game_over == 0:
+        if game_over == False:
             if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
                 clicked = True
             if event.type == pygame.MOUSEBUTTONUP and clicked == True:
                 clicked = False
                 pos = pygame.mouse.get_pos()
-                cell_x = pos[0]
-                cell_y = pos[1]
-                if markers[cell_x // 100][cell_y // 100] == 0:
-                    markers[cell_x // 100][cell_y // 100] = player
+                cell_x = pos[0] // 100
+                cell_y = pos[1] // 100
+                if markers[cell_x][cell_y] == 0:
+                    markers[cell_x][cell_y] = player
                     player *= -1
                     check_winner()
-    if game_over == True:
-        draw_winner(winner)
+
+    if game_over:
+        if winner != 0:
+            draw_winner(winner)
+        elif tie:
+            draw_tie()
+
         if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
             clicked = True
         if event.type == pygame.MOUSEBUTTONUP and clicked == True:
@@ -123,6 +151,7 @@ while run:
                 pos = []
                 player = 1
                 winner = 0
+                tie = False
                 game_over = False
                 for x in range(3):
                     row = [0] * 3
